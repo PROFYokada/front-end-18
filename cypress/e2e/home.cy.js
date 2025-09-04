437a338dfb8d00336dbeba4d8d1233c7997e24dd
PROFYokada <yokadavitor@gmail.com>
2025-09-04 13:35:38 -0300

    first commit

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,10 @@
+import Home from "./Home";
+
+describe("Página Home", () => {
+  it("deve carregar o título corretamente", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("Bem-vindo ao Projeto React!");
+  });
+});
+
+npm.start();
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Bem-vindo ao Projeto React!</h1>
+      <p>Este é um exemplo de página para testes com Cypress.</p>
+    </div>
+  );
+}
